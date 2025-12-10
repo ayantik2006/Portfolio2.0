6be0a311de22d28821c101e6d09c8e36816694c0
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-12-10 21:10:48 +0530

    responsiveness

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -45,7 +45,7 @@ function Hero() {
           </Link>
 
           <a
-            href="https://drive.google.com/file/d/1TmuYrMKZNFIZGZwdF0aLSo1ypNNkmlCT/view"
+            href="https://drive.google.com/file/d/1l44NDvExYtzTXSn4K5mkzCdn998oi-k-/view?usp=sharing"
             target="_blank"
           >
             <button className="bg-transparent text-white border-2 px-4 py-2 rounded-[2rem] font-bold hover:-translate-y-1 duration-300">
